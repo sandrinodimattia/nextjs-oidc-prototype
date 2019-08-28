463412521d3ce98c40fc905d84891ffdce48da2f
Luis Alvarez <luis@zeit.co>
2019-08-27 19:28:32 -0500

    Removed the loading state from about

--- examples/nextjs-auth-demo/pages/about.js
+++ examples/nextjs-auth-demo/pages/about.js
@@ -8,18 +8,11 @@ export default function About() {
   return (
     <Layout user={user} loading={loading}>
       <h1>Auth0 example</h1>
-
-      {loading ? (
-        <p>
-          Loading login info...
-        </p>
-      ) : (
-        <p>
-          This is the about page, navigations between this page and <i>Home</i> are
-          always pretty fast, <i>Profile</i> takes more time because it uses SSR to
-          fetch the user first
-        </p>
-      )}
+      <p>
+        This is the about page, navigations between this page and <i>Home</i> are
+        always pretty fast, <i>Profile</i> takes more time because it uses SSR to
+        fetch the user first
+      </p>
     </Layout>
   )
 }
